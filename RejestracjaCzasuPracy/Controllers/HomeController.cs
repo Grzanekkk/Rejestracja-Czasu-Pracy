bcd6a59f68e3d3de030d63837b5666eef7338855
Jan Puto <tofukles@gmail.com>
2020-10-22 14:07:33 +0200

    update

---
+++ RejestracjaCzasuPracy/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RejestracjaCzasuPracy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string path = "../TestPage";
+            return View(path);
+        }
+
+        public ActionResult Details()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RejestracjaCzasuPracy/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RejestracjaCzasuPracy.Controllers
+{
+    public class UserController : Controller
+    {
+        public string GetAllUSers()
+        {
+            return "All users"; 
+        }
+
+        public string GetUser(int id)
+        {
+            return $"User with id = {id}";
+        }
+    }
+}
