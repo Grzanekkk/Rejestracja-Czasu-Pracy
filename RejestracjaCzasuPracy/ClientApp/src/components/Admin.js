befcf3015d7e618e0b32f74c2576bd1fe9a55ab6
Tommy Phan <70032717+0xyy@users.noreply.github.com>
2020-10-23 12:28:48 +0200

    update

--- RejestracjaCzasuPracy/ClientApp/src/components/Admin.js
+++ RejestracjaCzasuPracy/ClientApp/src/components/Admin.js
@@ -1,30 +1,27 @@
 ﻿import React, { Component } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+
 
 
 export default class Admin extends Component {
     constructor(props) {
         super(props);
-        const token = localStorage.getItem("token");
-
-        let loggedIn = true;
-        if(token === null) {
-            loggedIn = false
-        }
-
         this.state = {
-            loggedIn
+
         }
     }
+
+    
     render() {
-        const { loggedIn } = this.state;
-        if(loggedIn === false) {
-            return <Redirect to="/logout"/>
-        }
         return (
+<<<<<<< Updated upstream
             <div>
                 <h1>Admin page</h1>
                 <Link to="/logout">Logout</Link>
+=======
+			<div>
+				<h1>Witaj {this.props.user}</h1>
+                <button onClick={this.props.onLoggOut} >Log out</button>
+>>>>>>> Stashed changes
             </div>
         )
     }
