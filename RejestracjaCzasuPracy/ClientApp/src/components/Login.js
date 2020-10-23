e04c76964f9a55425f18fdb69861f1580b2d238c
Tommy Phan <70032717+0xyy@users.noreply.github.com>
2020-10-23 12:40:06 +0200

    Revert "Merge branch 'Przykład-logowania'"
    This reverts commit 93e2d9ced234f20e407ddc421624a7bc00c46c88, reversing
    changes made to befcf3015d7e618e0b32f74c2576bd1fe9a55ab6.

--- RejestracjaCzasuPracy/ClientApp/src/components/Login.js
+++ RejestracjaCzasuPracy/ClientApp/src/components/Login.js
@@ -1,31 +1,65 @@
 ﻿import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+
+const ValidationMessage = (props) => {
+    return <p>{props.text}</p>
+}
 
 export class Login extends Component {
+<<<<<<< Updated upstream
+    state = {
+        loggedIn: false
+=======
 	constructor(props) {
-		super(props);
-		this.handleFormSubmit = this.handleFormSubmit.bind(this);
-	}
+        super(props);
+        this.state = {
+            userName: '',
+            isSelected: true,
+        }
+>>>>>>> Stashed changes
+    }
 
     handleFormSubmit = (e) => {
-		e.preventDefault();
-		this.props.onLoggIn("Jan Puto");
+        e.preventDefault();
+<<<<<<< Updated upstream
+        if(!this.state.loggedIn) {
+            localStorage.setItem("token", "x")
+            this.setState({
+                loggedIn: true,
+            })
+        }
+=======
+        if (this.state.userName) this.props.onLoggIn(this.state.userName);
+        else return this.setState({isSelected: false})
+    }
+
+    handleUserChange = (e) => {
+        this.setState({
+            userName: e.target.value,
+        })
+>>>>>>> Stashed changes
     }
 
     render() {
+        const { loggedIn } = this.state;
+        if(loggedIn) {
+            return <Redirect to ="/admin"/>
+            // return <Admin/>
+        }
         return (
             <div>
                 <h1>Sign In</h1>
-                <h2>Choose User</h2>
+                <h3>Choose User</h3>
                 <form onSubmit={this.handleFormSubmit}>
-                    <select id="myList">
-                        <option value="1">Jan Puto</option>
-                        <option value="2">Huan Putong</option>
-                        <option value="3">Johny Pute</option>
-                        <option value="4">Jenek Putea</option>
+                    <select value={this.state.userName} onChange={this.handleUserChange}>
+                        <option value=""></option>
+                        <option value="Jan Puto">Jan Puto</option>
+                        <option value="Huan Putong">Huan Putong</option>
+                        <option value="Johny Pute">Johny Pute</option>
+                        <option value="Jenek Putea">Jenek Putea</option>
                     </select>           
                     <br/>
                     <button type="submit">Log in</button> 
+                    {this.state.isSelected ? null : <ValidationMessage text="User is not selected"/> }
                 </form>
             </div>
         )
