d7ebb4f15a02e8535bc2d9d9ca832a4ca7585349
Tommy Phan <70032717+0xyy@users.noreply.github.com>
2020-10-23 12:40:22 +0200

    Revert "update"
    This reverts commit befcf3015d7e618e0b32f74c2576bd1fe9a55ab6.

--- RejestracjaCzasuPracy/ClientApp/src/components/Logout.js
+++ RejestracjaCzasuPracy/ClientApp/src/components/Logout.js
@@ -1,11 +1,12 @@
 ﻿import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 
 export default class Logout extends Component {
     render() {
         return (
             <div>
                 <h1>You have been logged out!</h1>
+                <Link to="/login">Login again</Link>
             </div>
         )
     }
